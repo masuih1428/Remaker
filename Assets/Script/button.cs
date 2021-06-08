547c463fd5d972a37f5880b3bd663b5ed2d1aa26
ryusei <sword25ryuu12@gmail.com>
2021-06-08 13:49:15 +0900

    タイトル編集

---
+++ Assets/Script/button.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //ƒ{ƒ^ƒ“‚ª‰Ÿ‚³‚ê‚½‚Ìˆ—
+    public void ButtonClick()
+    {
+        EditorApplication.Beep();
+    }
+}
