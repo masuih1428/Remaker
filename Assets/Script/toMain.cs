1d4006179406d1ccdb8d71e882c1702858760dd9
ayumu <ayugh58@icloud.com>
2021-06-15 14:46:30 +0900

    title

---
+++ Assets/Script/toMain.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//ボタンを使用するためUIとSceneManagerを使用ためSceneManagementを追加
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class toMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // ボタンをクリックするとBattleSceneに移動します
+    public void Onclick1()
+    {
+
+        SceneManager.LoadScene("ステージ選択画面");
+    }
+    public void Onclick2()
+    {
+        SceneManager.LoadScene("score");
+    }
+    public void Onclick3()
+    {
+        SceneManager.LoadScene("キャラ選択画面");
+    }
+    public void Onclick4()
+    {
+        SceneManager.LoadScene("生成・素材・付替");
+    }
+    public void Onclick5()
+    {
+        SceneManager.LoadScene("shop");
+    }
+}
