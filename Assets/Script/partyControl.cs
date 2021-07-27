ceffbc76f202dee9c9d9e2183218031316b042ac
ayumu <ayugh58@icloud.com>
2021-07-27 14:49:59 +0900

    パーティー編成確認

--- Assets/Script/partyControl.cs
+++ Assets/Script/partyControl.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class partyControl : MonoBehaviour
 {
+    public SaveData saveData;
+    public GameObject content1;
+    public GameObject content2;
+    public GameObject content3;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+            for (int j = 0; j < 8;j++) {
+                
+            }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void partySet(List<GameObject> party,int partyNum)
+    {
+        for (int j = 0; j < party.Count; j++)
+        {
+            
+        }
     }
 }
