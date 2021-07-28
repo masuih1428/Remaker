f93cfe53b361bfcc7177cce5deebe68dca6b22c7
ryusei <sword25ryuu12@gmail.com>
2021-07-28 13:55:46 +0900

    sozaihyouji

--- Assets/Script/statusImage.cs
+++ Assets/Script/statusImage.cs
@@ -7,6 +7,8 @@ public class statusImage : MonoBehaviour
 {
     public GameObject prefbImage;
     public GameObject image;
+    public GameObject panel;
+    private GameObject clickedGameObject;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +19,22 @@ public class statusImage : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
-        if (image.GetComponent<Image>().sprite == )
+        if (Input.GetMouseButtonDown(0))
         {
-            image.GetComponent<Image>().SetOpacity(1.0f);
+            clickedGameObject = null;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+            Debug.Log(hit2d);
+            clickedGameObject = hit2d.transform.gameObject;
+            Debug.Log(clickedGameObject);
+
+            if(hit2d && hit2d.transform.gameObject == panel)
+            {
+                image.GetComponent<Image>().sprite = prefbImage.GetComponent<Image>().sprite;
+                image.GetComponent<Image>().SetOpacity(1.0f);
+            }
+ 
         }
         image.GetComponent<Image>().sprite = prefbImage.GetComponent<Image>().sprite;
         //画像を不透明にシテ表示
