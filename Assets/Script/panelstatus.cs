3b1f3355f72516225242d441156a5393659810dd
masuih1428 <masuih1428@gmail.com>
2021-08-22 16:39:48 +0900

    デバックで不備があったものの修正

--- Assets/Script/panelstatus.cs
+++ Assets/Script/panelstatus.cs
@@ -38,31 +38,30 @@ public class Panelstatus : MonoBehaviour
             GameObject obj = nakami.transform.GetChild(1).gameObject;
             // Debug.Log(obj);
             Drop script = obj.GetComponent<Drop>();
-            for (int i = 0; i < sale.saveData.drops.Count; i++)
+
+            try
             {
-
-
-
-                if (sale.saveData.drops[i].GetComponent<Drop>().partName.Equals(script.partName))
+                for (int i = 0; i < sale.saveData.drops.Count; i++)
                 {
-                  //  Debug.Log(sale.saveData.drops[i].GetComponent<Drop>().rare);
-                  //  Debug.Log(script.rare);
-                    sale.a = i;
-                    sale.text.text = script.partName;
-                    break;
+
+
+
+                    if (sale.saveData.drops[i].GetComponent<Drop>().partName.Equals(script.partName))
+                    {
+                        //  Debug.Log(sale.saveData.drops[i].GetComponent<Drop>().rare);
+                        //  Debug.Log(script.rare);
+                        sale.a = i;
+                        sale.text.text = script.partName;
+                        break;
+                    }
                 }
+
+                //キャラ画像の表示
+                Debug.Log(sale.a);
+            } catch (Exception e)
+            {
+                Debug.Log(e);
             }
-
-            //キャラ画像の表示
-            Debug.Log(sale.a);
-
-
-
-
-
-
-
-
 
             GameObject obj1 = textPanel.transform.GetChild(1).gameObject;
             //ステータスパネルの内容の変更
