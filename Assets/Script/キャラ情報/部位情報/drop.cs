56ccf02b93ce5e287bd87d8cbe86071d03152732
masuih1428 <masuih1428@gmail.com>
2021-08-26 16:12:24 +0900

    no message

--- Assets/Script/キャラ情報/部位情報/drop.cs
+++ Assets/Script/キャラ情報/部位情報/drop.cs
@@ -29,9 +29,10 @@ public class Drop : MonoBehaviour
     public void rareStart()
     {
         //rareがnullの時のみ最初にレアリティを決める
-        Debug.Log("Start");
+        
         if (this.rare == "")
         {
+            Debug.Log("Start");
             //レア度がnullの場合のみレア度を乱数で作成
             float r1 = Random.value;//0.0f～1.0fまでの値
             float rare = 0.3f; //レア
